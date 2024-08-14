cff07ae6bd2dfd56696b148c81f9e1af22c5fdce
Neil <113860155+NK8998@users.noreply.github.com>
2024-08-14 16:02:17 +0300

    contact added

--- js/about.js
+++ js/about.js
@@ -1,26 +1,3 @@
-let options = {
-  root: null,
-  rootMargin: "0px",
-  threshold: 0,
-};
-
-const handleRevealAnimations = () => {
-  const observer = new IntersectionObserver((entries) => {
-    entries.forEach((el) => {
-      if (el.isIntersecting) {
-        el.target.classList.add("show");
-      } else {
-        return;
-        if (el.target.classList.contains("Hero-pattern")) return;
-        el.target.classList.remove("show");
-      }
-    });
-  }, options);
-
-  const pageImages = document.querySelectorAll(".animatable-img");
-  pageImages.forEach((image) => {
-    observer.observe(image);
-  });
-};
+const handleRevealAnimations = () => {};
 
 document.addEventListener("DOMContentLoaded", handleRevealAnimations);
---
+++ js/mod.js
@@ -0,0 +1,47 @@
+let options = {
+  root: null,
+  rootMargin: "0px",
+  threshold: 0,
+};
+
+const toggleNav = () => {
+  document.querySelector(".collapsable-nav").classList.toggle("show");
+};
+
+const handleResize = () => {
+  const gap = 25;
+  const cardWidth = 330;
+  const packageContainer = document.querySelector(".store-cards-width-referer");
+  if (!packageContainer) return;
+  const { width } = packageContainer.getBoundingClientRect();
+  const maxNum = Math.trunc(width / cardWidth);
+  const totalGap = (maxNum - 1) * gap;
+  const recalculatedWidth = cardWidth * maxNum + totalGap;
+  document.documentElement.style.setProperty("--store-cards-width", `${recalculatedWidth}px`);
+};
+
+const setUpEventListeners = () => {
+  const burger = document.querySelector(".hamburger-svg");
+  handleResize();
+  burger.addEventListener("click", toggleNav);
+  window.addEventListener("resize", handleResize);
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((el) => {
+      if (el.isIntersecting) {
+        el.target.classList.add("show");
+      } else {
+        return;
+        if (el.target.classList.contains("Hero-pattern")) return;
+        el.target.classList.remove("show");
+      }
+    });
+  }, options);
+
+  const pageImages = document.querySelectorAll(".animatable-img");
+  pageImages.forEach((image) => {
+    observer.observe(image);
+  });
+};
+
+document.addEventListener("DOMContentLoaded", setUpEventListeners);
