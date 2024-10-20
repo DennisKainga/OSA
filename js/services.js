16ee4e1a53667d88fbbcc827b70ec738157a89f5
Neil <113860155+NK8998@users.noreply.github.com>
2024-10-20 19:05:30 +0400

    jb

--- js/services.js
+++ js/services.js
@@ -3,14 +3,16 @@ const toggleCard = (index) => {
   const cards = document.querySelectorAll(".card");
   cards.forEach((card, i) => {
     card.classList.remove("active");
-    card.querySelector(".more-btn").textContent = `${index === i && curIndex !== index ? "Show Less" : "Show More"}`;
+    card.querySelector(".more-btn").textContent = `Show More`;
   });
 
   if (curIndex === index) {
+    curIndex = null;
     return;
   }
 
   cards[index].classList.toggle("active");
+  cards[index].querySelector(".more-btn").textContent = "Show Less";
   curIndex = index;
 };
 
